ff4a2c86b9598656ab79302b438542224332312e
Ramez Khojah <khojahramez29@gmail.com>
2026-02-23 01:17:31 +0300

    Revert "Implement Redis Streams for `insert` in `FeedDaoRedisImpl`."
    This reverts commit a617e1a6a623ce19bb2d09338fcefbcb35d9aaeb.

--- src/main/java/com/redislabs/university/RU102J/dao/FeedDaoRedisImpl.java
+++ src/main/java/com/redislabs/university/RU102J/dao/FeedDaoRedisImpl.java
@@ -1,13 +1,12 @@
 package com.redislabs.university.RU102J.dao;
 
 import com.redislabs.university.RU102J.api.MeterReading;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.StreamEntry;
-import redis.clients.jedis.StreamEntryID;
+import redis.clients.jedis.*;
 
+import java.nio.channels.Pipe;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FeedDaoRedisImpl implements FeedDao {
 
@@ -23,10 +22,6 @@ public class FeedDaoRedisImpl implements FeedDao {
     @Override
     public void insert(MeterReading meterReading) {
         // START Challenge #6
-        try (Jedis jedis = jedisPool.getResource()) {
-            jedis.xadd(RedisSchema.getGlobalFeedKey(), StreamEntryID.NEW_ENTRY, meterReading.toMap());
-            jedis.xadd(RedisSchema.getFeedKey(meterReading.getSiteId()), StreamEntryID.NEW_ENTRY, meterReading.toMap());
-        }
         // END Challenge #6
     }
 
